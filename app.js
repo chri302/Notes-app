35b318c96d9434999002d681893d0af14808d463
christineestur <esturchristine4@gmail.com>
2022-10-21 22:29:47 +0800

    delete.js and update.js added

--- app.js
+++ app.js
@@ -1,31 +1,44 @@
 const add = require('./add')
 const read = require('./read')
+const del = require('./del')
+const update = require('./update');
 
 
-//get users input
 const cmd = process.argv
 
 if (cmd[2] === 'add') {
-  // build the object
+  
   note = {
     "id": cmd[3],
     "title": cmd[4],
     "body": cmd[5]
   }
-
-  // get note.txt content
   var oldNote = read()
 
-  // add this newNote to Note.txt
   add(note, oldNote);
 }
-
-// Read Notes.txt
 if (cmd[2] === 'read')
-//import present
 
 var present = require('./present')
  {
   console.log(read());
 }
+if (cmd[2] === "delete") {
 
+  const id = cmd[3]
+  const oldNote = read ()
+  del(id, oldNote)
+  console.log(del)
+}
+if (cmd[2] === 'update') {
+  let note = {
+    id: cmd[3],
+    title: cmd[4],
+    body: cmd [5]
+  }
+
+const newNote = read ()
+update(note, newNote)
+console.log(update)
+
+}
